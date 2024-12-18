c2eecf5991cc5bbd14fa463b9f7c59e76deec679
Somnath Goswami <somnath.goswami@cginfinity.com>
2024-12-18 20:42:46 +0530

    added client secret and id

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "Ov23ligGatRQZTrN1bk1",
-      clientSecret: "8fa76081636e4b693da20a2b6d6a848aebc0c46d",
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
     }),
   ],
 };
